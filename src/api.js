8867772c119673a28376e017ffcc4b89646b5cf9
Jia Ming Liou <dliou471@gmail.com>
2018-03-18 14:53:01 -0600

    preflight fixed

--- src/api.js
+++ src/api.js
@@ -4,33 +4,27 @@ import Cookies from 'js-cookie';
 
 const baseURLMovieDB = "https://api.themoviedb.org/3/movie";
 const api_key = "?api_key=2a562783dd6f691cf5e7f97ecc5f9726";
+const axiosInstance = axios.create();
+axiosInstance.defaults.withCredentials = true;
+const axiosInstanceForMovieDB = axios.create();
+axiosInstanceForMovieDB.defaults.withCredentials = false;
 export default {
   user: {
-    login: credentials => {
-      axios.defaults.withCredentials = true;
-      return axios.post(`${baseURL}/api/auth`, { credentials }).then(res => res.data.user)
-    },
-    signup: user => axios.post(`${baseURL}/api/user`, { user }).then(res => res.data.user)
+    login: credentials => axiosInstance.post(`${baseURL}/api/auth`, { credentials }).then(res => res.data.user),
+    signup: user => axiosInstance.post(`${baseURL}/api/user`, { user }).then(res => res.data.user)
   },
   movies: {
-    init: () => {
-      axios.defaults.withCredentials = false;
-      console.log(axios.defaults.withCredentials)
-      return axios
-        .get(`${baseURLMovieDB}/popular${api_key}&&sort_by=popularity.desc`)
-        .then(res => res.data.results)
-    },
-    searchMovie: id => axios.get(`${baseURLMovieDB}/${id}${api_key}`)
+    init: () => axiosInstanceForMovieDB
+      .get(`${baseURLMovieDB}/popular${api_key}&&sort_by=popularity.desc`)
+      .then(res => res.data.results),
+    searchMovie: id => axiosInstanceForMovieDB.get(`${baseURLMovieDB}/${id}${api_key}`)
       .then(res => res.data),
-    saveMovie: movie => axios.post(`${baseURL}/api/movie`, { movie })
+    saveMovie: movie => axiosInstance.post(`${baseURL}/api/movie`, { movie })
       .then(res => res.data.movie),
-    personalCollection: userId => {
-      axios.defaults.withCredentials = true;
-      return axios
-        .post(`${baseURL}/api/movie/userCollection`, { userId })
-        .then(res => res.data)
-    },
-    deleteMovie: (_id, userId) => axios.post(`${baseURL}/api/movie/delete/${_id}`, { userId })
+    personalCollection: userId => axiosInstance
+      .post(`${baseURL}/api/movie/userCollection`, { userId })
+      .then(res => res.data),
+    deleteMovie: (_id, userId) => axiosInstance.post(`${baseURL}/api/movie/delete/${_id}`, { userId })
       .then(res => res.data)
   }
 };
